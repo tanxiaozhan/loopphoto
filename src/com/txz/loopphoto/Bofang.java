066195aa1415b0321ae9371fbef8ae6a8ec7d378
txz <tanxiaozhan@gmail.com>
2013-09-30 16:18:44 +0800

    sc

--- src/com/txz/loopphoto/Bofang.java
+++ src/com/txz/loopphoto/Bofang.java
@@ -12,6 +12,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Environment;
 import android.util.AttributeSet;
@@ -36,6 +37,8 @@ public class Bofang extends View {
     
     int photoCount=0;  //图片总数
     String[] strFileName;
+    
+    String photoInfo="";
 
 
     
@@ -53,6 +56,10 @@ public class Bofang extends View {
         		
         }
         
+        //控件的宽，高
+        COMPONENT_WIDTH = this.getWidth();  
+        COMPONENT_HEIGHT = this.getHeight();  
+
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
         scheduler.scheduleWithFixedDelay(new runner(), 0, 500, TimeUnit.MILLISECONDS);
 
@@ -89,19 +96,51 @@ public class Bofang extends View {
     }  
     */
     
-    //** 加载本地图片   
+    
+	public int calculateInSampleSize(BitmapFactory.Options options,  
+	        int reqWidth, int reqHeight) {  
+	    // 源图片的高度和宽度  
+	    final int height = options.outHeight;  
+	    final int width = options.outWidth;  
+	    int inSampleSize = 1;  
+	    if (height > reqHeight || width > reqWidth) {  
+	        // 计算出实际宽高和目标宽高的比率  
+	        final int heightRatio = Math.round((float) height / (float) reqHeight);  
+	        final int widthRatio = Math.round((float) width / (float) reqWidth);  
+	        // 选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高  
+	        // 一定都会大于等于目标的宽和高。  
+	        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+	        photoInfo=String.valueOf(inSampleSize);
+	    }  
+	    return inSampleSize;  
+	}
+	
+	public Bitmap decodeSampledBitmapFromFile(String url) {  
+	    // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小  
+	    final BitmapFactory.Options options = new BitmapFactory.Options();  
+	    options.inJustDecodeBounds = true;  
+	    BitmapFactory.decodeFile(url);  
+	    // 调用上面定义的方法计算inSampleSize值  
+	    options.inSampleSize = calculateInSampleSize(options, COMPONENT_WIDTH, COMPONENT_HEIGHT );  
+	    // 使用获取到的inSampleSize值再次解析图片  
+	    options.inJustDecodeBounds = false;  
+	    return BitmapFactory.decodeFile(url) ;  
+	}  
+	
+	//** 加载本地图片   
     public Bitmap getLoacalBitmap(String url) {
-    	try {
-    		FileInputStream fis = new FileInputStream(url);
-    		return BitmapFactory.decodeStream(fis);
-    	} 
-    	catch (FileNotFoundException e) {
-    		e.printStackTrace();
-    		return null;
-    	}
+    		//FileInputStream fis = new FileInputStream(url);
+    		//return BitmapFactory.decodeStream(fis);
+    		return decodeSampledBitmapFromFile(url); 
+    	 
     }
     
  
+    
+    
+    
+    
+    
     //覆写onDraw方法  
     @Override 
     protected void onDraw(Canvas canvas)   
@@ -114,13 +153,14 @@ public class Bofang extends View {
     	
         if(initflag)//检查是否已经获得图片
         {  
-            COMPONENT_WIDTH = this.getWidth();  
+        	Paint paint=new Paint();
+        	COMPONENT_WIDTH = this.getWidth();  
             COMPONENT_HEIGHT = this.getHeight();  
         	photoWidthHeightRate=bmp.getScaledWidth(canvas) / bmp.getScaledHeight(canvas);
             photoRect=new RectF(0,0,COMPONENT_WIDTH,(int)(COMPONENT_WIDTH/photoWidthHeightRate));
             //canvas.drawBitmap(bmp, 0, 0, null);//绘制图片
     		canvas.drawBitmap(bmp,null,photoRect,null);
-            //canvas.drawText("width",10,50,null);
+    		canvas.drawText(photoInfo,10,50,paint);
     		initflag=false;
 
         }  
